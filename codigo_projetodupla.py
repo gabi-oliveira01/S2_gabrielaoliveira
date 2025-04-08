dead879d02fa0517deb3dfc633e54de6abcb0cde
Agatha-Helena <aghata.m@aluno.senai.br>
2025-04-08 10:54:36 -0300

    Update codigo_projetodupla.py

--- codigo_projetodupla.py
+++ codigo_projetodupla.py
@@ -2,14 +2,22 @@
 
     if a % b == 0 :
         return True
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> parent of ce9502d (Merge branch 'incluir1')
         return False
 
 
 valor = int(input("digite um valor:"))
+<<<<<<< HEAD
+
+=======
+>>>>>>> parent of ce9502d (Merge branch 'incluir1')
 
 
-resultado= multiplo (valor, valor2)
+resultado= multiplo (valor, valor)
 
 if resultado:
     print("os numeros são multiplos")
