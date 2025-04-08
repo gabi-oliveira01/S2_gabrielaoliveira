52d9c9aab25bb212afa8d01e868bba9fce136b7a
Agatha-Helena <aghata.m@aluno.senai.br>
2025-04-08 10:52:18 -0300

    projeto original
    modificação para o original

--- codigo_projetodupla.py
+++ codigo_projetodupla.py
@@ -1,17 +1,20 @@
 # -*- coding: UTF-8 -*-
-def multiplo (a,b):
+
     if a % b == 0 :
         return True
-    else:
+
         return False
 
 
 valor = int(input("digite um valor:"))
-valor2=int(input("digite outro valor:"))
+
 
 resultado= multiplo (valor, valor2)
 
 if resultado:
     print("os numeros são multiplos")
-else:
-    print("não são multiplos")
+
+
+
+
+
