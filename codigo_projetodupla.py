e8f25b989e24a83807c4c4fa3b4ef8af7e7292a4
gabi-oliveira01 <gabriela.d.oliveira10@aluno.senai.br>
2025-04-08 10:58:08 -0300

    Update codigo_projetodupla.py

--- codigo_projetodupla.py
+++ codigo_projetodupla.py
@@ -2,19 +2,11 @@
 
     if a % b == 0 :
         return True
-<<<<<<< HEAD
-
-=======
     
->>>>>>> parent of ce9502d (Merge branch 'incluir1')
         return False
 
 
 valor = int(input("digite um valor:"))
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of ce9502d (Merge branch 'incluir1')
 
 
 resultado= multiplo (valor, valor)
