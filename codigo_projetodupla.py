2a452303fcd8b82e697ee29e0a2064266e268c9b
gabi-oliveira01 <gabriela.d.oliveira10@aluno.senai.br>
2025-04-08 09:07:20 -0300

    modificado pasta codigo_projetodupla.py
    modificado

--- codigo_projetodupla.py
+++ codigo_projetodupla.py
@@ -1,18 +1,21 @@
 # -*- coding: UTF-8 -*-
-
+def multiplo (a,b):
     if a % b == 0 :
         return True
-    
+    else:
         return False
 
 
-valor = int(input("digite um valor:"))
+valor =(input("digite um valor:"))
+valor2=int(input("digite outro valor:"))
 
-
-resultado= multiplo (valor, valor)
+resultado= (valor, valor)
 
 if resultado:
     print("os numeros são multiplos")
+else:
+    print("não são multiplos")
+
 
 
 
